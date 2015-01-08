ea1dc277140d4922b3857eb4094e9f9c3fb32530
jose alvarado <jose.alvarado@move.com>
2015-01-08 08:44:02 -0800

    adding 7th floor rooms

--- maps/roomService.js
+++ maps/roomService.js
@@ -23,6 +23,8 @@ Office.prototype = {
 }
 
 var office = new Office("San Jose");
+
+//8th Floor
 office.addRoom(new Room("penthouse", true, 4, true, true));
 office.addRoom(new Room("bungalow", false, 4, true, true));
 office.addRoom(new Room("houseboat", true, 4, true, true));
@@ -31,6 +33,20 @@ office.addRoom(new Room("wigwam", true, 4, true, true));
 office.addRoom(new Room("castle", false, 4, true, true));
 office.addRoom(new Room("farmhouse", true, 4, true, true));
 office.addRoom(new Room("adobe", false, 4, true, true));
+//7th Floor
+office.addRoom(new Room("gazebo", true, 4, true, true));
+office.addRoom(new Room("igloo", false, 4, true, true));
+office.addRoom(new Room("mansion", false, 4, true, true));
+office.addRoom(new Room("ued_lab", true, 4, true, true));
+office.addRoom(new Room("ued_viewing", true, 4, true, true));
+office.addRoom(new Room("lighthouse", false, 4, true, true));
+office.addRoom(new Room("tepee", true, 4, true, true));
+office.addRoom(new Room("yurt", false, 4, true, true));
+office.addRoom(new Room("casa", true, 4, true, true));
+office.addRoom(new Room("treehouse", true, 4, true, true));
+office.addRoom(new Room("chalet", false, 4, true, true));
+office.addRoom(new Room("loft", false, 4, true, true));
+office.addRoom(new Room("arena", false, 4, true, true));
 
 var moveOffices = {};
 moveOffices[office.name] = office;
