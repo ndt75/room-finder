cc0ffed05fcbaa84fe03005241e1c99ce7f5969d
jose alvarado <jose.alvarado@move.com>
2015-01-07 16:50:04 -0800

    maps folder

---
+++ maps/sanJoseCtrl.js
@@ -0,0 +1,4 @@
+app.controller("sanJoseCtrl", function($scope) {
+    $scope.dynamicPopover = 'Hello, World!';
+  	$scope.dynamicPopoverTitle = 'Title';
+});
---
+++ maps/sanJoseDirective.js
@@ -0,0 +1,43 @@
+'use strict';
+
+app.directive("room", function () {
+    var config = {
+        replace: true,
+        transclude: false,
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var shape = $(element);
+            var name = shape.data("name");
+            var template = 
+            "<div>" + 
+            "<ul class='details'>" +
+            "<li><label>Name:</label><span>" + name +"</span></li>" + 
+            "<li><label>People:</label><span>" + 4 +"</span></li>" + 
+            "<li><label>Video</label><span>" + "yes" +"</span></li>" +  
+            "<li><label>Times</label><span>" + "yes" +"</span></li>" +  
+            "</ul>" +
+            "</div>";
+
+            var options = {
+                trigger: "hover",
+                delay: { show: 100, hide: 120},
+                content: template,
+                placement: "left",
+                html: true
+            };
+
+            shape.popover(options);
+    		
+    	  	var available = shape.data("available");
+            
+
+        	var fill = (available) ? "#EDE6CE" : "#F15A22";
+            //var style = (available) ? "availableA" : "unavailableA";
+
+            //shape.addClass(style);
+            shape.css("fill", fill);
+        }
+    };
+
+    return config;
+});
