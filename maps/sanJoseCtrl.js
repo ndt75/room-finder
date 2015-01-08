67d809170e2629858426e8d239ea69d20e9bb770
jose alvarado <jose.alvarado@move.com>
2015-01-07 18:50:29 -0800

    all working

--- maps/sanJoseCtrl.js
+++ maps/sanJoseCtrl.js
@@ -1,10 +1,66 @@
 app.controller("sanJoseCtrl", function($scope) {
-    var rooms = [];
-	rooms["penthouse"] = {
+    var rooms = {
+	    "penthouse" : {
 	    	name: "penthouse", 
-	    	available: false
-	    };
+	    	available: false,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"bungalow" : {
+	    	name: "bungalow", 
+	    	available: true,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"houseboat" : {
+	    	name: "houseboat", 
+	    	available: false,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"lanai" : {
+	    	name: "lanai", 
+	    	available: true,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"wigwam" : {
+	    	name: "wigwam", 
+	    	available: false,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"castle" : {
+	    	name: "castle", 
+	    	available: true,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"farmhouse" : {
+	    	name: "farmhouse", 
+	    	available: false,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    },
+		"adobe" : {
+	    	name: "adobe", 
+	    	available: true,
+	    	people: 4,
+	    	video: true,
+	    	whiteboard: true
+	    }
+	}
 	$scope.rooms = rooms;
 
-    console.log(rooms.penthouse.points);
+	$scope.hasAttribute = function(value)
+	{
+		return (value) ? "yes" : "no";
+	}
 });
--- maps/sanJoseDirective.js
+++ maps/sanJoseDirective.js
@@ -7,15 +7,16 @@ app.directive("room", function () {
         restrict: 'A',
         link: function (scope, element, attrs) {
             var shape = $(element);
-            var name = shape.data("name");
             var attrs = JSON.parse(attrs.params);
+
             var template = 
             "<div>" + 
             "<ul class='details'>" +
             "<li><label>Name:</label><span>" + attrs.name +"</span></li>" + 
-            "<li><label>People:</label><span>" + 4 +"</span></li>" + 
-            "<li><label>Video</label><span>" + "yes" +"</span></li>" + 
-            "<li><label>White Board</label><span>" + "yes" +"</span></li>" +
+            "<li><label>Available:</label><span>" + scope.hasAttribute(attrs.available) +"</span></li>" + 
+            "<li><label>People:</label><span>" + attrs.people +"</span></li>" + 
+            "<li><label>Video</label><span>" + scope.hasAttribute(attrs.video) +"</span></li>" + 
+            "<li><label>White Board</label><span>" + scope.hasAttribute(attrs.whiteboard) +"</span></li>" +
             "</ul>" +
             "</div>";
 
@@ -28,14 +29,8 @@ app.directive("room", function () {
             };
 
             shape.popover(options);
-    		
-    	  	var available = shape.data("available");
-            
 
-        	var fill = (available) ? "#EDE6CE" : "#F15A22";
-            //var style = (available) ? "availableA" : "unavailableA";
-
-            //shape.addClass(style);
+        	var fill = (attrs.available) ? "#EDE6CE" : "#F15A22";
             shape.css("fill", fill);
         }
     };
